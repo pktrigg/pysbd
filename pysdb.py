aeffc54609de3895bd52a603a7e1d333f70afc8d
pk <pktrigg@gmail.com>
2023-12-27 20:42:45 -0300

    improve readme

--- pysdb.py
+++ pysdb.py
@@ -36,25 +36,6 @@ def main():
 	print("Complete reading SDB file :-)")
 	reader.close()
 
-# ###############################################################################
-# def createxyz(reader, projection, step=0):
-# 	lastTimeStamp = 0
-# 	navigation = reader.loadNavigation()
-
-# 	if len(navigation) == 0:
-# 		return
-
-# 	# dump data to xyz
-# 	for update in navigation:
-# 		if update.timestamp - lastTimeStamp >= step:
-# 			if projection is not None:
-# 				x,y = projection(float(update.shipX),float(update.shipY))
-# 				print("%.10f, %.10f, %.3f, %.3f" % (x, y, update.sensorAux1, update.sensorAux2))
-# 			else:
-# 				print("%.10f, %.10f, %.3f, %.3f" % (update.shipX, update.shipY, update.sensorAux1, update.sensorAux2))
-# 			# print("%s %.10f, %.10f, %.3f, %.3f" % (update.dateTime, update.shipX, update.shipY, update.sensorAux1, update.sensorAux2))
-# 			lastTimeStamp = update.timestamp
-
 ###############################################################################
 # TIME HELPER FUNCTIONS
 ###############################################################################
@@ -63,12 +44,6 @@ def to_timestamp(dateObject):
 
 def from_timestamp(unixtime):
 	return datetime.utcfromtimestamp(unixtime)
-
-def dateToKongsbergDate(dateObject):
-	return dateObject.strftime('%Y%m%d')
-
-def dateToKongsbergTime(dateObject):
-	return dateObject.strftime('%H%M%S')
 
 def dateToSecondsSinceMidnight(dateObject):
 	return (dateObject - dateObject.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
@@ -172,133 +147,22 @@ class SBDFILEHDR:
 
 			self.sensors[sensorname] = sensor
 
-
-		#not sure we need this....
-		# pkppkpkpk
+		#not sure why we need this....
 		data = fileptr.read(4)
 
-		print (fileptr.tell())
-
-
-
-		# #NOW DO THE SECOND RECORD...
-
-		# print ("XX %d" % (fileptr.tell()))
-
-		# #now lets try to read the data packet header which is 20 bytes...
-		# hdr_fmt = '=18h' # 36 bytes according to CARIS dump program
-		# hdr_len = struct.calcsize(hdr_fmt)
-		# hdr_unpack = struct.Struct(hdr_fmt).unpack_from
-
-		# data = fileptr.read(hdr_len)
-		# s = hdr_unpack(data)
-		
-		# msgid 			= s[0]
-		# msglen 			= s[16]
-		# msgsubpackets 	= s[2]
-
-		# print (fileptr.tell())
-		# # Disk Loc: 0x1228 offset 4648
-		# msg_fmt = '=' + str(msglen) + 's'
-		# msg_len = struct.calcsize(msg_fmt)
-		# msg_unpack = struct.Struct(msg_fmt).unpack_from
-
-		# print (fileptr.tell())
-		# data = fileptr.read(msg_len)
-		# s = msg_unpack(data)
-		# msg=s[0].decode('utf-8').rstrip('\x00')
-		# print(msg)
-		# print(msg)
-
-
-
-
-		# # self.fileSize = fileptr.seek(4770, 0) # as per CARIS...
-		# #not sure wht we need these....
-		# # data = fileptr.read(10)
-		# print ("YY %d" % (fileptr.tell()))
-
-
-		# #now lets try to read the data packet header which is 20 bytes...
-		# msg_fmt = '=20h' # 40 bytes we think
-		# msg_len = struct.calcsize(msg_fmt)
-		# msg_unpack = struct.Struct(msg_fmt).unpack_from
-
-		# data = fileptr.read(msg_len)
-		# s = msg_unpack(data)
-		
-
-		# #s[8] seems to be the packet length
-		# msgid 			= s[0]
-		# msglen 			= s[1]
-		# msglen 			= s[8] #this is correct!!!!
-		# msgsubpackets 	= s[2] 
-
-		# #first nmea string at 4682
-		# msg_fmt = '=' + str(msglen) + 's'
-		# msg_len = struct.calcsize(msg_fmt)
-		# msg_unpack = struct.Struct(msg_fmt).unpack_from
-
-		# print (fileptr.tell())
-		# data = fileptr.read(msg_len)
-		# s = msg_unpack(data)
-		# print(s[0])
-		# print(s[0])
-
-#########################################################################################
+	#########################################################################################
 	def __str__(self):
 		return (pprint.pformat(vars(self)))
 
-class Packet61Reader:
-	P61Header_fmt = '=2H 4L 10B H 2L 2H L 2H 3L'
-	P61Header_len = struct.calcsize(P61Header_fmt)
-	P61Header_unpack = struct.Struct(P61Header_fmt).unpack_from
-
-	P61Checksum_fmt = '=L'
-	P61Checksum_len = struct.calcsize(P61Checksum_fmt)
-	P61Checksum_unpack = struct.Struct(P61Checksum_fmt).unpack_from
-
-	def __init__(self, fileptr):
-		data = fileptr.read(self.P61Header_len)
-		s = self.P61Header_unpack(data)
-
-		self.version 					= s[0]
-		self.offset 					= s[1]
-		self.syncPattern				= s[2]
-		self.size	 					= s[3]
-		self.dataOffset					= s[4]
-		self.dataIdentifier				= s[5]
-		self.s7kTime 					= s[6:16]
-		self.recordVersion				= s[17]
-		self.deviceIdentifier			= s[18]
-		self.reserved					= s[19]
-		self.systemEnumerator			= s[20]
-		self.reserved2					= s[21]
-		self.flags						= s[22]
-		self.reserved3					= s[23]
-		self.reserved4					= s[24]
-		self.totalRecordsinFragmentedSet= s[25]
-		self.fragmentNumber				= s[26]
-
-		# now read the dynamic part of the record.  we need to compute the bytes to read from the size minus the header and checksum
-		bytesTosRead = self.size - self.P61Header_len - self.P61Checksum_len
-		self.dataSection = fileptr.read(bytesTosRead)
-
-		# now read the checksum
-		data = fileptr.read(self.P61Checksum_len)
-		s = self.P61Checksum_unpack(data)
-		self.checksum = s[0]
-
+#########################################################################################
 class SBDReader:
-	# SDBPacketHeader_fmt = '=h2b3hL'
-	# SDBPacketHeader_len = struct.calcsize(SDBPacketHeader_fmt)
-	# SDBPacketHeader_unpack = struct.Struct(SDBPacketHeader_fmt).unpack_from
-
-	#now lets try to read the data packet header which is 32 bytes...
-	hdr_fmt = '=16h'
+	'''now lets try to read the data packet header which is 32 bytes...'''
+	hdr_fmt = '=16h' # we know this works....
+	hdr_fmt = '=3l4f2h'
 	hdr_len = struct.calcsize(hdr_fmt)
 	hdr_unpack = struct.Struct(hdr_fmt).unpack_from
 
+	#########################################################################################
 	def __init__(self, SDBfileName):
 		if not os.path.isfile(SDBfileName):
 			print ("file not found:", SDBfileName)
@@ -315,20 +179,24 @@ class SBDReader:
 		#the file is of a sensible size so open it.
 		self.SDBFileHdr = SBDFILEHDR(self.fileptr)
 
+	#########################################################################################
 	def __str__(self):
 		return pprint.pformat(vars(self))
 
+	#########################################################################################
 	def close(self):
 		self.fileptr.close()
 
+	#########################################################################################
 	def rewind(self):
 		# go back to start of file
 		self.fileptr.seek(0, 0)
 		self.SDBFileHdr = SBDFILEHDR(self.fileptr)
 
+	#########################################################################################
 	def moreData(self):
 		bytesRemaining = self.fileSize - self.fileptr.tell()
-		print ("current file ptr position:", self.fileptr.tell())
+		# print ("current file ptr position:", self.fileptr.tell())
 		return bytesRemaining
 
 	# def loadNavigation(self):
@@ -347,6 +215,7 @@ class SBDReader:
 	# 	# print("Get navigation Range Duration %.3fs" % (time.time() - start_time)) # print the processing time.
 	# 	return (navigation)
 
+	#########################################################################################
 	def computeSpeedFromPositions(self, navData):
 		if (navData[0].sensorX <= 180) & (navData[0].sensorY <= 90): #data is in geographicals
 			for r in range(len(navData) - 1):
@@ -371,22 +240,7 @@ class SBDReader:
 
 		return meanSpeed, navData
 
-	def readDatagramheader(self):
-		data = self.fileptr.read(self.SDBPacketHeader_len)
-		if len(data) != self.SDBPacketHeader_len:
-			return 0,0,0,0
-		s = self.SDBPacketHeader_unpack(data)
-
-		MagicNumber				= s[0]
-		HeaderType				= s[1]
-		SubChannelNumber		= s[2]
-		NumChansToFollow		= s[3]
-		Reserved1				= s[4]
-		Reserved2				= s[5]
-		NumBytesThisRecord		= s[6]
-
-		return HeaderType, SubChannelNumber, NumChansToFollow, NumBytesThisRecord
-
+	#########################################################################################
 	def readDatagram(self):
 		ping = None
 		# remember the start position, so we can easily comput the position of the next packet
@@ -410,7 +264,6 @@ class SBDReader:
 		# Datagram type: GYRO (9.0)           Time: 1683658758.107 (2023/05/09 18:59:18.107),  Size:     30,  # subpackets:    0
 		# Seqence Number:    0  Gyro:  37.950
 
-
 		#not sure wht we need these....
 		# data = fileptr.read(4)
 
@@ -419,8 +272,13 @@ class SBDReader:
 		data = self.fileptr.read(self.hdr_len)
 		s = self.hdr_unpack(data)
 
-		msgid 			= s[0]
-		msglen 			= s[4] - 20
+		# msgid 			= s[0]
+		msgunixtimeseconds 			= s[0]
+		msgunixtimemicroseconds 			= s[1]
+		msgtimestamp = msgunixtimeseconds + (msgunixtimemicroseconds / 1000000)
+		msglen 						= s[2] - 20 #we know this works...!!!!
+
+		# msglen 			= s[4] - 20 #we know this works...!!!!
 		msgsubpackets 	= s[2]
 
 		# print (self.fileptr.tell())
@@ -435,39 +293,11 @@ class SBDReader:
 		# msg=s[0].decode('utf-8').rstrip('\x00')
 		print(s[0])
 
+		return ping
 
-		# # read the packet header.  This permits us to skip packets we do not support
-		# HeaderType, SubChannelNumber, NumChansToFollow, NumBytesThisRecord = self.readDatagramheader()
-		# # trap corrup record
-		# if NumBytesThisRecord == 0:
-		# 	return ping
-
-		# if HeaderType == 0:
-		# 	ping = SDBPINGHEADER(self.fileptr, self.SDBFileHdr, SubChannelNumber, NumChansToFollow, NumBytesThisRecord)
-
-		# 	# now read the padbytes at the end of the packet
-		# 	padBytes = currentPacketPosition + NumBytesThisRecord - self.fileptr.tell()
-		# 	if padBytes > 0:
-		# 		data = self.fileptr.read(padBytes)
-		# if HeaderType == 61:
-		# 	ping = SDBPINGHEADER(self.fileptr, self.SDBFileHdr, SubChannelNumber, NumChansToFollow, NumBytesThisRecord)
-		# 	bathy = Packet61Reader(self.fileptr)
-		# 	# now read the padbytes at the end of the packet
-		# 	padBytes = currentPacketPosition + NumBytesThisRecord - self.fileptr.tell()
-		# 	# padBytes = (self.fileptr.tell() - currentPacketPosition) % 64
-		# 	print ("Ping: %s  record for system:%s type:%s" % (ping.PingNumber, bathy.deviceIdentifier, bathy.recordVersion))
-		# 	if padBytes > 0:
-		# 		# bathy = Packet61Reader(self.fileptr) # this does not seem to work.  Odd!
-		# 		data = self.fileptr.read(padBytes)
-		# else:
-		# 	# print ("unsupported packet type:%s at byte offset:%s NumBytes:%s" % (HeaderType, currentPacketPosition, NumBytesThisRecord))
-		# 	self.fileptr.seek(currentPacketPosition + NumBytesThisRecord, 0)
-
-		# return ping
-
-	# def readChannel(self):
-	#	 return SDBPINGCHANHEADER()
-
+#########################################################################################
+#########################################################################################
+#########################################################################################
 if __name__ == "__main__":
 	main()
 
